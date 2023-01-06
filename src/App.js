3340f95c189dcec81037e1ebafe0d1c1b75c7b58
StefanDjaleski <stefan_djaleski@epam.com>
2023-01-06 17:59:19 +0100

    calorie app started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [calories, setCalories] = useState("");
+
+  useEffect(() => {
+    console.log(calories);
+  }, [calories]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Calorie calculator app</header>
+      <input
+        type="text"
+        onChange={(event) => setCalories(event.target.value)}
+      />
     </div>
   );
 }
