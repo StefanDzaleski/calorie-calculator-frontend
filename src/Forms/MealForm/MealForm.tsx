15d4be68028454f598a2c904f95ab0a6b97a45a9
StefanDzaleski <stefan.dzaleski@gmail.com>
2023-01-24 19:08:29 +0100

    meal type added

--- src/Forms/MealForm/MealForm.tsx
+++ src/Forms/MealForm/MealForm.tsx
@@ -1,24 +1,25 @@
 import { Formik, Form, FieldArray } from "formik";
+import axiosInstance from "../../axiosInstance";
 import TextInput from "../../FormItems/TextInput/TextInput";
 
 function MealForm() {
   const addMeal = (values: any, helpers: any) => {
-    console.log("values", values);
-    // axiosInstance
-    //   .post("/add-meal", { values })
-    //   .then(function (response) {
-    //     console.log(response);
-    //     helpers.resetForm({ values: {ingredients: ['']} });
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
+    axiosInstance
+      .post("/add-meal", { values })
+      .then(function (response) {
+        console.log(response);
+        helpers.resetForm({ values: { type: "", ingredients: [""] } });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
-    <Formik onSubmit={addMeal} initialValues={{ ingredients: [""] }}>
+    <Formik onSubmit={addMeal} initialValues={{ type: "", ingredients: [""] }}>
       {({ values }) => (
         <Form>
+          <TextInput name={`type`} />
           <FieldArray
             name="ingredients"
             render={(arrayHelpers) => (
--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://calorie-calculator-backend.vercel.app",
+  // baseURL: "https://calorie-calculator-backend.vercel.app",
+  baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
     timeout: 1000,
